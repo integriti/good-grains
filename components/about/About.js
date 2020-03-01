92e706409f21a571013bebd74a0c2ee427d6c68a
integriti <integriti@gmail.com>
2020-03-01 22:32:45 +1100

    Stackblitz sucks

--- components/about/About.js
+++ components/about/About.js
@@ -43,10 +43,11 @@ const WhiteBackground = styled.div`
   max-width: 120rem;
   text-align: left;
   font-size: 14px;
+  padding: 0.5em;
 
   @media (max-width: 768px) {
-    padding: 1em;
     font-size: 1em;
+    padding: 1em;
   }
 `;
 
@@ -56,9 +57,10 @@ const Creations = styled.div`
   max-width: 120rem;
   text-align: right;
   font-size: 14px;
+  padding: 0.5em;
 
   @media (max-width: 768px) {
-    padding: 1em;
     font-size: 1em;
+    padding: 1em;
   }
 `;
