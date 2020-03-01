b07da1353e04f2db45b34e3301d63907836a2d86
integriti <integriti@gmail.com>
2020-03-01 18:28:05 +1100

    Props WIP 2

--- components/home/Carousel.js
+++ components/home/Carousel.js
@@ -7,46 +7,54 @@ const Carousel = props => {
   return (
     <section class="carousel" aria-label="Gallery">
       <ol class="carousel__viewport">
-        <li id="carousel__slide1" tabindex="0" class="carousel__slide">
-          <Image src="https://i.imgur.com/iA71mhT.png" alt="Foxtail" />;
-          <div class="carousel__snapper">
-            <a href="#carousel__slide4" class="carousel__prev" />
-            <a href="#carousel__slide2" class="carousel__next" />
-          </div>
-        </li>
-        <li id="carousel__slide2" tabindex="0" class="carousel__slide">
-          <Image src="https://i.imgur.com/5ZX6Ae3.png" alt="Kodo" />;
-          <div class="carousel__snapper" />
-          <a href="#carousel__slide1" class="carousel__prev" />
-          <a href="#carousel__slide3" class="carousel__next" />
-        </li>
-        <li id="carousel__slide3" tabindex="0" class="carousel__slide">
-          <Image src="https://i.imgur.com/OPRfLW7.png" alt="Foxtail" />;
-          <div class="carousel__snapper" />
-          <a href="#carousel__slide2" class="carousel__prev" />
-          <a href="#carousel__slide4" class="carousel__next" />
-        </li>
-        <li id="carousel__slide4" tabindex="0" class="carousel__slide">
-          <Image src="https://i.imgur.com/BeWWn3S.png" alt="Kodo" />;
-          <div class="carousel__snapper" />
-          <a href="#carousel__slide3" class="carousel__prev" />
-          <a href="#carousel__slide1" class="carousel__next" />
-        </li>
+        {(function() {
+          var products = [];
+          switch (props.type) {
+            case "millets":
+              products = millets;
+              break;
+            case "shakes":
+              products = shakes;
+              break;
+          }
+
+          var list = [];
+          for (var i = 0; i < products.length; i++) {
+            var href = "#carousel__slide" + (i + 1);
+            list.push(
+              <li id="carousel__slide1" tabindex="0" class="carousel__slide">
+                <Image src={millets[i]} alt="Foxtail" />;
+                <div class="carousel__snapper" />
+              </li>
+            );
+          }
+          return list;
+        })()}
       </ol>
       <aside class="carousel__navigation">
         <ol class="carousel__navigation-list">
-          <li class="carousel__navigation-item">
-            <a href="#carousel__slide1" class="carousel__navigation-button" />
-          </li>
-          <li class="carousel__navigation-item">
-            <a href="#carousel__slide2" class="carousel__navigation-button" />
-          </li>
-          <li class="carousel__navigation-item">
-            <a href="#carousel__slide3" class="carousel__navigation-button" />
-          </li>
-          <li class="carousel__navigation-item">
-            <a href="#carousel__slide4" class="carousel__navigation-button" />
-          </li>
+          {(function() {
+            var products = [];
+            switch (props.type) {
+              case "millets":
+              products = millets;
+              break;
+            case "shakes":
+              products = shakes;
+              break;
+            }
+
+            var list = [];
+                for (var i = 0; i < products.length; i++) {
+                  var href = "#carousel__slide" + (i + 1);
+                  list.push(
+                    <li class="carousel__navigation-item">
+                      <a href={href} class="carousel__navigation-button" />
+                    </li>
+                  );
+                }
+                return list;
+          })()}
         </ol>
       </aside>
     </section>
